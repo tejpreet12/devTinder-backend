f7aa5b7d79392da91c68f06df80180e36d8a4028
tejpreet12 <tejpreetdhiman429@gmail.com>
2025-04-23 22:31:07 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const app = express();
 const PORT = 7777;
-const HOSTNAME = '127.0.0.1';
+const HOSTNAME = "127.0.0.1";
 
-
-app.get("/user/:userId/:gender",(req,res) => {
-    console.log(req.params,"params");
-    res.json({
-        name:"Tejpreet Singh",
-        code:"NTZ 2044",
-    })
-})
-
+app.use(
+  "/user",
+  (req, res, next) => {
+    next();
+    // res.send("HELLO");
+  },
+  (req, res, next) => {
+    // res.send("HELLO 2");
+    next();
+  },
+  (req, res, next) => {
+    res.send("HELLO 3");
+  }
+);
 
 app.listen(PORT, () => {
-    console.log("Server start on port 7777");
-})
+  console.log("Server start on port 7777");
+});
