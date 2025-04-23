c6249e5fa200c44c40418f2efc9cb5a243bba5b5
tejpreet12 <tejpreetdhiman429@gmail.com>
2025-04-23 21:53:26 +0530

    Routing+RequestHandlers

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 7777;
 const HOSTNAME = '127.0.0.1';
 
-app.use("/test", (req,res) =>{
-    res.status(200).json({
+
+app.get("/user/:userId/:gender",(req,res) => {
+    console.log(req.params,"params");
+    res.json({
         name:"Tejpreet Singh",
-        EmployeeCode:"NTZ 2044",
+        code:"NTZ 2044",
     })
 })
 
 
-app.listen(PORT, HOSTNAME, () => {
+app.listen(PORT, () => {
     console.log("Server start on port 7777");
 })
