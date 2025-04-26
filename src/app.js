d289f8063d569c18b0ec24639dfa38d70b51cc37
tejpreet12 <tejpreetdhiman429@gmail.com>
2025-04-26 09:55:25 +0530

    Error Handling

--- src/app.js
+++ src/app.js
@@ -5,15 +5,34 @@ const HOSTNAME = "127.0.0.1";
 
 const { authMiddleware } = require("./middlewares/auth");
 
+//Global Error Handler
+// app.use("/", (err,req,res,next) => {
+//       if(err){
+//         res.status(500).send("Some Error Occured");
+//       }
+// })
+
+// Auth Middleware
 app.use("/admin", authMiddleware);
+
 
 app.use("/admin/getAllData", (req, res) => {
   res.status(200).json({ data: ["All DATA"] });
 });
 
 app.use("/user", (req, res) => {
-  res.status(200).json({ data: ["USER DATA"] });
+  throw new Error();
+  // res.status(200).json({ data: ["USER DATA"] });
 });
+
+// Global Error Handler
+// The order of code matters here because we will not get error handled if we have global error
+// handler at top so it is recommended to error the global error handler as the last route
+app.use("/", (err,req,res,next) => {
+  if(err){
+    res.status(500).send("Some Error Occured");
+  }
+})
 
 app.listen(PORT, () => {
   console.log("Server start on port 7777");
