c8a545be66ee73086af23c865545fe8edef2df8d
tejpreet12 <tejpreetdhiman429@gmail.com>
2025-05-01 23:03:10 +0530

    Diving into the api's

--- src/app.js
+++ src/app.js
@@ -5,21 +5,113 @@ const User = require("./models/user.js");
 const PORT = 7777;
 const HOSTNAME = "127.0.0.1";
 
-app.post("/signup", async (req, res) => {
-  const userObj = {
-    name: "Joe",
-    lastName: "Root",
-    emailId: "joe@mail.com",
-    password: "Test@123",
-    age: 33,
-    gender: "Male",
-  };
+app.use(express.json());
 
-  const user = new User(userObj);
+// /user for single user with emailId
+
+app.get("/user", async (req, res) => {
+  const email = req.body?.emailId;
 
   try {
+    const user = await User.findOne({ emailId: email });
+
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: `No user found` });
+    }
+  } catch (err) {
+    res.status(400).json({ message: `Something went wrong : ${err.message}` });
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (deletedUser) {
+      res.json({ message: "User delete successfully" });
+    } else {
+      res.status(404).json({ message: `No user found` });
+    }
+  } catch (err) {
+    res.status(400).json({ message: `Something went wrong : ${err.message}` });
+  }
+});
+
+// app.patch("/user", async (req, res) => {
+//   const userId = req.body.userId;
+
+//   try {
+//     const updatedUser = await User.findByIdAndUpdate(
+//       { _id: userId },
+//       req.body,
+//       { returnDocument: "before" }
+//     );
+//     if (updatedUser) {
+//       res.json({ message: "User update successfully", updatedUser });
+//     } else {
+//       res.status(404).json({ message: `No user found` });
+//     }
+//   } catch (err) {
+//     res.status(400).json({ message: `Something went wrong : ${err.message}` });
+//   }
+// });
+
+// patch with emailID
+app.patch("/user", async (req, res) => {
+  const emailId = req.body.emailId;
+
+  try {
+    const updatedUser = await User.findOneAndUpdate({ emailId }, req.body);
+    if (updatedUser) {
+      res.json({ message: "User update successfully", updatedUser });
+    } else {
+      res.status(404).json({ message: `No user found` });
+    }
+  } catch (err) {
+    res.status(400).json({ message: `Something went wrong : ${err.message}` });
+  }
+});
+
+// /feed for all users
+
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find();
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: `No user found` });
+    }
+  } catch (err) {
+    res.status(400).json({ message: `Something went wrong : ${err.message}` });
+  }
+});
+
+// HW findByID
+
+app.get("/user/:ID", async (req, res) => {
+  const userId = req.params.ID;
+
+  try {
+    const user = await User.findById(userId);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: `No user found` });
+    }
+  } catch (err) {
+    res.status(400).json({ message: `Something went wrong : ${err.message}` });
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  try {
     await user.save();
-    res.json({ message: `User Created` });
+    res.json({ message: `New User Created` });
   } catch (err) {
     res.status(400).json({ message: `Unable to create user ${err.message}` });
   }
