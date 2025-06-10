0b8033896704c6a68c3ba64e29f32c0a3ea02cf0
Tejpreet12 <tejpreetdhiman429@gmail.com>
2025-06-10 23:23:11 +0530

    FeedApi-Pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const { authMiddleware } = require("../middlewares/auth");
 const {
   ConnectionRequestModel: ConnectionRequest,
 } = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 const SAFE_POPULATE_KEYS = "firstName lastName";
@@ -41,7 +42,6 @@ userRouter.get("/user/connections", authMiddleware, async (req, res) => {
       .populate("fromUserId", SAFE_POPULATE_KEYS)
       .populate("toUserId", SAFE_POPULATE_KEYS);
 
-
     const data = connectionRequests.map((data) => {
       if (data.fromUserId._id.toString() === loggedInUser._id.toString()) {
         return data.toUserId;
@@ -58,6 +58,46 @@ userRouter.get("/user/connections", authMiddleware, async (req, res) => {
     res
       .status(400)
       .json({ message: `ERROR: ${err.message}`, status: "Not Ok" });
+  }
+});
+
+userRouter.get("/feed", authMiddleware, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = req.query.page || 1;
+    let limit = req.query.limit || 10;
+
+    limit = limit > 50 ? 50 : limit;
+    
+    const skip = (page - 1) * limit;
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    });
+
+    const hideUsersFromFeed = new Set();
+
+    connectionRequest.map((data) => {
+      hideUsersFromFeed.add(data.fromUserId.toString());
+      hideUsersFromFeed.add(data.toUserId.toString());
+    });
+
+    const user = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select("firstName lastName")
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ message: "Feed data", users: user, status: "Ok" });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: `ERROR : ${err.message}`, status: "Not Ok" });
   }
 });
 
